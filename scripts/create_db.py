f8084f0c2b9904a7e38e4dc18770c0ceeace3859
Annu-Singh-CSE <annusingh12112003@gmail.com>
2026-07-14 17:24:08 +0530

    database create

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -9,21 +9,19 @@ def create_database():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        port=int(os.getenv("DB_PORT", 3306))
+        port=int(os.getenv("DB_PORT",3306))
     )
 
     try:
         with connection.cursor() as cursor:
             cursor.execute(f"CREATE DATABASE IF NOT EXISTS {os.getenv('DB_NAME')};")
             connection.commit()
-            print(f"Database : {os.getenv('DB_NAME')} is create or exist")
-
+            print(F"Database : {os.getenv("DB_NAME")} is create or exists ✅" )
     except Exception as e:
-        print("Error:", e)
+        print("Error :",e)
 
     finally:
         connection.close()
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     create_database()
